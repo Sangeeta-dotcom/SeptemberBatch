a36d2cc5cab56ec5cf3617f7cc8e67d242516a68
admin <admin@DESKTOP-35G3Q1F.lan>
2024-10-29 14:49:51 +0530

    push

---
+++ Practice4/src/main/java/Practice4/Practice4/App.java
@@ -0,0 +1,13 @@
+package Practice4.Practice4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Practice4/src/test/java/Package1/Sanjay.java
@@ -0,0 +1,9 @@
+package Package1;
+
+public class Sanjay {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+
+}
